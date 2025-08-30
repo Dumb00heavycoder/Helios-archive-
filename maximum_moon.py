3fc72f04ba2258500e5fb3c482032f6a0c62e460
dhruv <dhruvlgd44@gmail.com>
2025-08-31 00:25:08 +0530

    maximum number of moons done

--- maximum_moon.py
+++ maximum_moon.py
@@ -1,4 +1,10 @@
 def maximum_moon(df):
-    print(df['Number of Moons'].max())
+    max_moons = df['Number of Moons'].max()
+
+    planet_name = df[df['Number of Moons'] == max_moons]['Planet Name'].tolist()
+    if len(planet_name) > 1:
+         print('Planets with maximum number of moons are', planet_name, 'with', max_moons, 'moons')
+    else:
+        print('Planet with maximum number of moons is', planet_name, 'with', max_moons, 'moons')
 
 
