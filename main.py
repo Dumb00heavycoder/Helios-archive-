1a00a84a06887aae8195cae2007ff96ba237932e
dhruv <dhruvlgd44@gmail.com>
2025-08-31 00:59:22 +0530

    structuring main.py

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ df = pd.read_csv('Solarsystem_planet_data.csv')
 
 from maximum_moon import maximum_moon
 
+print('Welcome to the Helios Archive! This is an educational passion project I’ve created as a fun way to pass the time. I’ve included a solar system dataset with this project, but feel free to use your own data as well.') 
+
+#will give planet with max moon
 maximum_moon(df)
+
+#will give average distance from sum of all planets
+
+'''will give distance between earth and other planets with the help of average distance from sun '''
+
+#will give average, min and max orbital period
+
+#still finding what to do with rp, shape, sg, and esccape velocity
+
+
+
+
+
 
