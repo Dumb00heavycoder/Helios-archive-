e115ec2956369c315c5dc27c18dd5fa456e2170e
dhruv <dhruvlgd44@gmail.com>
2025-09-05 15:46:06 +0530

    ..

--- orbital_period.py
+++
@@ -1,6 +0,0 @@
-def orbital_period(df):
-    avg = df['Orbital Period (years)'].mean()
-    mini = df['Orbital Period (years)'].min()
-    maxi = df ['Orbital Period (years)'].max()
-    print (avg, mini, maxi,) 
-
