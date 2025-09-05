6f608c07b313e650590d11a64ea263470d2254f4
dhruv <dhruvlgd44@gmail.com>
2025-09-05 15:44:13 +0530

    edited first model and main file

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from scripts.orbital_period import orbital_period
 
 #will give planet with max moon
 maximum_moon(df)
+print('\n')
 #will give average distance from sum of all planetis
 average_distance(df)
+print('\n')
 #will give average, min and max orbital period
 orbital_period(df)
 #still finding what to do with rp, shape, sg, and esccape velocity
--- scripts/maximum_moon.py
+++ scripts/maximum_moon.py
@@ -1,10 +1,11 @@
 def maximum_moon(df):
     max_moons = df['Number of Moons'].max()
 
-    planet_name = df[df['Number of Moons'] == max_moons]['Planet Name'].tolist()
-    if len(planet_name) > 1:
-         print('Planets with maximum number of moons are', planet_name, 'with', max_moons, 'moons')
+    planets_name = df[df['Number of Moons'] == max_moons]['Planet Name'].tolist()
+    planet_name = df[df['Number of Moons'] == max_moons]['Planet Name'].iloc[0]
+    if len(planets_name) > 1:
+         print(f"Planets with maximum number of moons are {planets_name} with {max_moons} moons")
     else:
-        print('Planet with maximum number of moons is', planet_name, 'with', max_moons, 'moons')
+        print(f"Planet with maximum number of moons is {planet_name} with {max_moons} moons")
 
 
