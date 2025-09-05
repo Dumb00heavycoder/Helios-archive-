9ac97e008102972a3527311ec44fd69a31dec417
dhruv <dhruvlgd44@gmail.com>
2025-09-05 14:41:23 +0530

    script 3

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 df = pd.read_csv('Solarsystem_planet_data.csv')
 
 from maximum_moon import maximum_moon
-from average_distance_from_star import average_distance 
+from average_distance_from_star import average_distance
+from orbital_period import orbital_period
 
 #will give planet with max moon
 maximum_moon(df)
@@ -12,6 +13,7 @@ maximum_moon(df)
 average_distance(df)
 
 #will give average, min and max orbital period
+orbital_period(df)
 
 #still finding what to do with rp, shape, sg, and esccape velocity
 
