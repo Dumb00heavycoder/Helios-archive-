957608b17af87d92533a9b85d79256c0b4dcdfe9
dhruv <dhruvlgd44@gmail.com>
2025-09-05 20:42:35 +0530

    new task

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ average_distance(df)
 print('\n')
 #will give average, min and max orbital period
 orbital_period(df)
-#still finding what to do with rp, shape, sg, and esccape velocity
+print('\n')
+#will give heaviest and lighest planet with which is hardest to escape and easiest to escape
 
+#will give travel time for sunlight, which takes max which takes least and what is average
 
+#shape comparison. which is most perfect and which is least perfect  
 
+#communication delay by computing light travel time using distance and speed of light. 
+
+#calculate orbital speed of all planets
+
+#
 
 
 
